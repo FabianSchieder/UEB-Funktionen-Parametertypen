aace637c80a3514ee92293e21ae93efeb56f618e
Fabian Schieder <129980914+FabianSchieder@users.noreply.github.com>
2023-12-19 18:38:54 +0100

    Update checkPalindrome.cpp

--- checkPalindrome.cpp
+++ checkPalindrome.cpp
@@ -28,7 +28,7 @@ bool checkPalindrome(string word)
 {
     for (int i = 0; i < word.length() / 2; i++)
     {
-        if (word[i] != word.at(word.length() - 1 - i))
+        if (word.at(i) != word.at(word.length() - 1 - i))
         {
             return false;
         }
